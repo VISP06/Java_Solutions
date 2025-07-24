76d8020dc478a505b4654c6ebdbf77357d524c7a
Vishruth <notvishruth@gmail.com>
2025-07-24 21:52:21 +0530

    Reverse a Linked List

---
+++ src/ReverseLinkedList.java
@@ -0,0 +1,32 @@
+/*
+Intellij might show an error where it says that it
+cannot access the ListNode but that is most likely a bug
+because the code runs just fine
+Also make sure for the ListNode file to be present along side this to run
+*/
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        ListNode reversedhead=reverseList(head);
+        ListNode temp=reversedhead;
+        while(temp!=null){
+            System.out.print(temp.val+"->");
+            temp=temp.next;
+        }
+        System.out.print("NULL");
+    }
+    static public ListNode reverseList(ListNode head) {
+        ListNode prev=null;
+        ListNode curr=head;
+        while(curr!=null){
+            ListNode future=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=future;
+            if(future!=null) {
+                future = future.next;
+            }
+        }
+        return prev;
+    }
+}
