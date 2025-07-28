0704d25c3e3786a5f467484fffbc09bffbb8b62b
Vishruth <notvishruth@gmail.com>
2025-07-28 17:51:21 +0530

    Length of the last word

---
+++ src/LengthOfTheLastWord.java
@@ -0,0 +1,12 @@
+public class LengthOfTheLastWord {
+    public static void main(String[] args) {
+        String sentence="  alpha   beta   gamma   delta   -  ";
+        System.out.println(lengthOfLastWord(sentence));
+    }
+    static public int lengthOfLastWord(String s) {
+        s=s.trim();
+        int lastSpace=s.lastIndexOf(' ');
+        String lastWord = s.substring(lastSpace+1);
+        return lastWord.length();
+    }
+}
