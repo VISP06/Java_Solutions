4320c91ffa1634b4617f8bdede6deba504ae4003
Vishruth <notvishruth@gmail.com>
2025-07-30 23:17:33 +0530

    Merge Sorted Array

---
+++ src/MergeSortedArray.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{4, 8, 15, 0, 0, 0};
+        int[] nums2 = new int[]{5, 9, 12};
+        int m=3;
+        int n=3;
+        System.out.println(Arrays.toString(merge(nums1, m, nums2, n)));
+    }
+    static public int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        int index=0;
+        for(int i=nums1.length-1;i>m-1;i--){
+            if(nums1[i]==0){
+                nums1[i]=nums2[index];
+                index++;
+            }
+        }
+        Arrays.sort(nums1);
+        return nums1;
+    }
+}
